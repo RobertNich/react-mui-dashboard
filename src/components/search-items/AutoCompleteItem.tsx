e2ca0e1d0ceeaf157a1f6a80a0be9748865dda3e
Robert <ro13ertnichols@gmail.com>
2023-07-19 19:00:26 +0930

    added auto complete item for search pages

---
+++ src/components/search-items/AutoCompleteItem.tsx
@@ -0,0 +1,46 @@
+import { Autocomplete, CircularProgress, TextField } from "@mui/material";
+import { useState } from "react";
+
+type Props = {
+  label: string;
+  data: any[];
+  helpText: string;
+};
+
+export const AutoCompleteItem = ({ label, data, helpText }: Props) => {
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (event: any) => {
+    setInput(event.target.value);
+  };
+
+  const loading = data.length === 0;
+  return (
+    <Autocomplete
+      id="asynchronous-demo"
+      sx={{ width: 300 }}
+      options={data}
+      onChange={(event) => handleInputChange(event)}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={label}
+          value={input}
+          helperText={helpText}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+};
